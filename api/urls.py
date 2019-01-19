ef9e1c99e85b427fe68c1460f8f1e287b45a83db
Matt Krabacher <matt.krabacher@gmail.com>
2019-01-18 16:47:26 -0800

    updated url format and added movie reccomendation search

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^json/artist/(?P<artist>\b.+\b)', views.getMusicArtist_json),
-    url(r'^json/genre/artists/(?P<genre>\b.+\b)', views.getMusicGenreArtists_json),
-    url(r'^json/genre/tracks/(?P<genre>\b.+\b)', views.getMusicGenreTracks_json),
+    url(r'^json/music/artist/(?P<artist>\b.+\b)', views.getMusicArtist_json),
+    url(r'^json/music/genre/artists/(?P<genre>\b.+\b)', views.getMusicGenreArtists_json),
+    url(r'^json/music/genre/tracks/(?P<genre>\b.+\b)', views.getMusicGenreTracks_json),
+    url(r'^json/movie/(?P<movie>\b.+\b)(&release=)(?P<release>\d+)?', views.getMovieRecomendations_json),
+    url(r'^json/movie/(?P<movie>\b.+\b)', views.getMovieRecomendations_json),
 ]
