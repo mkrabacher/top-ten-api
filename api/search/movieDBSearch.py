49347ce0792aa54ebf7e621993d29e4bb83e8767
Matt Krabacher <matt.krabacher@gmail.com>
2019-01-21 11:43:56 -0800

    added trailers to similar movie results

--- api/search/movieDBSearch.py
+++ api/search/movieDBSearch.py
@@ -7,29 +7,44 @@ movieDBkey = movieDBkey()
 resultsLimit = 10
 
 
-def getMovieFromString(query, releaseYear=False):
+def getMovieFromString(query, release_year=False, trailer=False):
     url = "https://api.themoviedb.org/3/search/movie?"
     url += "api_key=" + movieDBkey
     url += "&language=en-US="
     url += "&query=" + query
     url += "&page=1"
     url += "&include_adult=false"
-    if releaseYear:
-      url += "&primary_release_year=" + str(releaseYear)
+
+    if release_year:
+      url += "&primary_release_year=" + str(release_year)
+
     
     movieRes = urllib.urlopen(url)
     movieData = json.loads(movieRes.read())
     
+    
     if movieData['results'][0]['title'].lower() != query.lower():
       movieData['error'] = 'No exact match found'
+      if trailer:
+        for movie in movieData['results']:
+          movie['trailer'] = youtube_search(movie['name'])
+  
       return movieData
 
-    return movieData['results'][0]
+    if trailer:
+      movieData['results'][0]['trailer'] = youtube_search(movieData['results'][0]['name'])
 
-def getRecommendedMoviesFromString(query, releaseYear=False):
+    return movieData
 
-  movieData = getMovieFromString(query, releaseYear=False)
-  
+def getRecommendedMoviesFromString(query, release_year=False, trailer=False):
+  movieData = getMovieFromString(query, release_year, False)
+
+  if 'error' in movieData:
+    return movieData
+  else:
+    movieData = movieData['results'][0]
+
+
   url = "https://api.themoviedb.org/3/movie/"
   url += str(movieData["id"])
   url += "/recommendations?"
@@ -39,10 +54,18 @@ def getRecommendedMoviesFromString(query, releaseYear=False):
   
   similarMovieRes = urllib.urlopen(url)
   similarMovieData = json.loads(similarMovieRes.read())
+  similarMovieData = similarMovieData['results'][:10]
+
+  if trailer:
+    for movie in similarMovieData:
+      search_term = movie['title']
+      search_term += ' movie trailer'
+      print 'searching youtube for ' + search_term
+      movie['trailer'] = youtube_search(search_term)
 
   result = {
     'movie': movieData,
-    'similar': similarMovieData['results'][:10]
+    'similar': similarMovieData
   }
 
   return result
