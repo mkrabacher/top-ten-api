c4753c9230e0b3dfd2aafb3175d031a8eafb29c7
Matt Krabacher <matt.krabacher@gmail.com>
2019-01-21 11:43:17 -0800

    updated movie search params handleing

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,5 @@ urlpatterns = [
     url(r'^json/music/artist/(?P<artist>\b.+\b)', views.getMusicArtist_json),
     url(r'^json/music/genre/artists/(?P<genre>\b.+\b)', views.getMusicGenreArtists_json),
     url(r'^json/music/genre/tracks/(?P<genre>\b.+\b)', views.getMusicGenreTracks_json),
-    url(r'^json/movie/(?P<movie>\b.+\b)(&release=)(?P<release>\d+)?', views.getMovieRecomendations_json),
-    url(r'^json/movie/(?P<movie>\b.+\b)', views.getMovieRecomendations_json),
+    url(r'^json/movie/(?P<query>\b.+\b)', views.getMovieRecomendations_json),
 ]
--- api/views.py
+++ api/views.py
@@ -23,7 +23,19 @@ def getMusicGenreTracks_json(request, genre):
   results = getBestOfGenreTracks(genre)
   return JsonResponse(results)
 
-def getMovieRecomendations_json(request, movie, release=False):
-  results = getRecommendedMoviesFromString(movie, release)
+def getMovieRecomendations_json(request, query):
+  params = query.split('&')
+  movie = ''
+  release = False
+  trailer = False
+  for param in params:
+    elements = param.split('=')
+    if elements[0] == 'movie':
+      movie = elements[1]
+    if elements[0] == 'release':
+      release = elements[1]
+    if elements[0] == 'trailer':
+      trailer = elements[1]
   
+  results = getRecommendedMoviesFromString(movie, release, trailer)
   return JsonResponse(results)
